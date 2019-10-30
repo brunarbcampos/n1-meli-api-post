f7f2b3f4d3be584ff13199c7523d23f05e80a64e
brunarbcampos <brunarbcampos@gmail.com>
2019-10-30 20:59:16 -0300

    update 30/10

---
+++ src/routes/alunasRoute.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+/*const alunas = require('../model/alunas.json')*/
+const controller = require ('../controllers/alunasController')
+
+router.get('/', controller.get)
+router.get ('/nasceuSp', controller.getSp)
+/*router.get('/', function(request, response){
+    response.status(200).send(alunas)
+}) */ //tiramos pq o controller puxa isso agora
+router.get ('/:id', controller.getById)
+router.get ("/:id/books", controller.getBooks)
+router.get ('/:id/getIdade', controller.getIdade)
+router.post("/", controller.post)
+router.post("/:id/books", controller.postBooks)
+
+module.exports =  router
